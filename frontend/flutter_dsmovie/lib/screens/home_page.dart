3d9c3a170581c755de206f7df7d446227912fffa
41SergioF <carlossergio2401@outook.com>
2022-03-13 18:49:36 -0300

    Connected with API -- For page

--- frontend/flutter_dsmovie/lib/screens/home_page.dart
+++ frontend/flutter_dsmovie/lib/screens/home_page.dart
@@ -15,7 +15,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Movie _movie;
-  Container _body = Container(color:Colors.grey,);
+  late List<Movie> _movies;
+
+  Widget _body = Center(
+    child: Container(
+      color: Colors.grey,
+    ),
+  );
 
   void getMovieById(int id) async {
     var request = http.Request('GET',
@@ -27,39 +33,62 @@ class _HomePageState extends State<HomePage> {
       Map map = jsonDecode(await response.stream.bytesToString());
       setState(() {
         _movie = Movie.fromMap(map);
-        _body = Container(child: MovieCard(_movie),);
+        _body = Container(child: MovieCard(_movie));
+      });
+    } else {}
+  }
+
+  void getMovies() async {
+    var request = http.Request(
+        'GET',
+        Uri.parse(
+            'https://carlos-sergio-dsmovie.herokuapp.com/movies?size=12&page=0'));
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      Map map = jsonDecode(await response.stream.bytesToString());
+
+      setState(() {
+        _movies = map['content'].map<Movie>((map) {
+          return Movie.fromMap(map);
+        }).toList();
+        _body = Container(
+            child: ListView.builder(
+          itemCount: _movies.length,
+          itemBuilder: (contex, index) {
+            return MovieCard(_movies[index]);
+          },
+        ));
       });
     } else {}
   }
 
   @override
   void initState() {
-    getMovieById(5);
+    getMovies();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF4d41c0),
-        title: const Text('DSMovie'),
-        actions: [
-          Image.asset('assets/images/github.png'),
-          GestureDetector(
-            onTap: () {},
-            child: const Padding(
-              padding: EdgeInsets.only(right: 10),
-              child: Align(
-                child: Text('/41SergioF'),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF4d41c0),
+          title: const Text('DSMovie'),
+          actions: [
+            Image.asset('assets/images/github.png'),
+            GestureDetector(
+              onTap: () {},
+              child: const Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: Align(
+                  child: Text('/41SergioF'),
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: _body,
-      ),
-    );
+          ],
+        ),
+        body: _body);
   }
 }
